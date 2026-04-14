d65d270f583e26617e34737c9a81230efe3089aa
unknown <asakeeva15.07@gmail.com>
2026-04-15 01:17:44 +0600

    add ai

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
 	// baseUrl: "https://back.unikurs.kg",
-	// baseUrl: "http://localhost:8080",
-	baseUrl: 'http://10.234.46.31:8080',
+	baseUrl: "http://localhost:8080",
+	// baseUrl: 'http://10.234.46.31:8080',
 }
